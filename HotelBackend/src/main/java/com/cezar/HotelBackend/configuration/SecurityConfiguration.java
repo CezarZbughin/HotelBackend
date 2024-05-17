08a35d81ff4b93c6ab974b4f34070f6ed0755a1f
Cezar Zbughin <cezarzbughin3@gmail.com>
2024-05-18 01:06:15 +0300

    make login end point

--- HotelBackend/src/main/java/com/cezar/HotelBackend/configuration/SecurityConfiguration.java
+++ HotelBackend/src/main/java/com/cezar/HotelBackend/configuration/SecurityConfiguration.java
@@ -3,10 +3,12 @@ package com.cezar.HotelBackend.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -39,9 +41,12 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                //.authorizeHttpRequests(auth->auth.requestMatchers("/**").permitAll())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req->req
+                        .requestMatchers("api/v1/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
                 .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
-                .authorizeHttpRequests(auth->auth.anyRequest().authenticated())
                 .httpBasic(withDefaults());
         return http.build();
 
--- HotelBackend/src/main/java/com/cezar/HotelBackend/controller/HelloController.java
+++ HotelBackend/src/main/java/com/cezar/HotelBackend/controller/HelloController.java
@@ -3,12 +3,19 @@ package com.cezar.HotelBackend.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class HelloController {
     @GetMapping("/hello")
     public ResponseEntity<?> hello(){
         return ResponseEntity.ok(new WebMessage("Hello World"));
+    }
+
+    @PostMapping("/hello")
+    public ResponseEntity<?> hellopost(@RequestBody WebMessage webMessage){
+        return ResponseEntity.ok(new WebMessage("Hello World post"));
     }
 
 }
