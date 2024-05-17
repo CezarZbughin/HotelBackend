77a0c029c7e886f879825e07e38a35dceb3a5d8b
Cezar Zbughin <cezarzbughin3@gmail.com>
2024-05-17 23:18:34 +0300

    Fix Type Args Order

--- HotelBackend/src/main/java/com/cezar/HotelBackend/repository/HotelRepository.java
+++ HotelBackend/src/main/java/com/cezar/HotelBackend/repository/HotelRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HotelRepository extends JpaRepository<Long, Hotel> {
+public interface HotelRepository extends JpaRepository<Hotel, Long> {
 }
