a3ff269436e3595b32536b18d5fa72dce485fff4
Cezar Zbughin <cezarzbughin3@gmail.com>
2024-05-18 06:05:37 +0300

    Added restrictions to SecurityFilterChain

--- HotelBackend/src/main/java/com/cezar/HotelBackend/service/HotelService.java
+++ HotelBackend/src/main/java/com/cezar/HotelBackend/service/HotelService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class HotelService {
@@ -30,6 +31,9 @@ public class HotelService {
         return hotelRepository.findAll();
     }
 
+    public Optional<Hotel> getById(long id) {
+        return hotelRepository.findById(id);
+    }
     /**
      *
      * @param lat Latitude of the center point
--- HotelBackend/src/main/java/com/cezar/HotelBackend/service/ReviewService.java
+++ HotelBackend/src/main/java/com/cezar/HotelBackend/service/ReviewService.java
@@ -1,2 +1,44 @@
-package com.cezar.HotelBackend.service;public class ReviewService {
+package com.cezar.HotelBackend.service;
+
+import com.cezar.HotelBackend.model.EndUser;
+import com.cezar.HotelBackend.model.Hotel;
+import com.cezar.HotelBackend.model.Review;
+import com.cezar.HotelBackend.repository.ReviewRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ReviewService {
+    @Autowired
+    ReviewRepository reviewRepository;
+    @Autowired
+    HotelService hotelService;
+
+    public List<Review> getAll(){
+        return reviewRepository.findAll();
+    }
+
+    public List<Review> getByHotel(Hotel hotel){
+        return reviewRepository.findAll()
+                .stream()
+                .filter(review -> review.getId().equals(hotel.getId()))
+                .toList();
+    }
+
+    public List<Review> getByHotelId(long hotelId) {
+        Hotel hotel = hotelService.getById(hotelId).orElseThrow(EntityNotFoundException::new);
+        return getByHotel(hotel);
+    }
+
+    public Review createReview(EndUser user, Hotel hotel, int rating, String comment){
+        Review review = new Review();
+        review.setUser(user);
+        review.setHotel(hotel);
+        review.setRating(rating);
+        review.setComment(comment);
+        return reviewRepository.save(review);
+    }
 }
